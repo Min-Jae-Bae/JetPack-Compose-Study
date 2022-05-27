6b5819d9f4111303829909a5b1dd555de9c328fc
Min-Jae-Bae <qoalswo123@icloud.com>
2022-05-27 15:09:25 +0900

    Good Compose !!

--- app/src/main/java/com/cookandroid/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/cookandroid/jetpackcompose/MainActivity.kt
@@ -1,16 +1,28 @@
 package com.cookandroid.jetpackcompose
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Text
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.cookandroid.jetpackcompose.ui.theme.JetPackComposeTheme
@@ -30,42 +42,38 @@ class MainActivity : ComponentActivity() {
 /*Reusing Composable
 * 상태 호이스팅*/
 @Composable
-fun MyApp(): Unit {
-    var shouldShowOnboarding by remember { mutableStateOf(true)}
+private fun MyApp(): Unit {
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
 
+
+    /*이벤트를 전달
+    * continue 버튼을 클릭했을 때 앱에 알림*/
     if (shouldShowOnboarding) {
-        OnboardingScreen()
+        OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
     } else {
         Greetings()
     }
 }
 
-@Composable
-private fun Greetings(names: List<String> = listOf("World", "Compose")): Unit {
-    Column(modifier = Modifier.padding(vertical = 4.dp)) {
-        for (name in names) {
-            Greeting(name = name)
-        }
-    }
-}
 
 /*새 컴포저블과 새 미리보기 추가*/
 @Composable
-fun OnboardingScreen(): Unit {
-
-    /* = 기호는 by 키워드를 대신 사용하여 .value를 입력할 필요가 없다.*/
-    var shouldShowOnboarding by remember { mutableStateOf(true) }
-
+private fun OnboardingScreen(
+    onContinueClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
     androidx.compose.material.Surface {
-        Column (
-            modifier = Modifier.fillMaxSize(),
+        Column(
+            modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState()),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text("Hello")
             Button(
                 modifier = Modifier.padding(vertical = 24.dp),
-                onClick = { shouldShowOnboarding = false }
+                onClick = onContinueClicked
             ) {
                 Text(text = "Continue")
             }
@@ -73,59 +81,111 @@ fun OnboardingScreen(): Unit {
     }
 }
 
-@Preview(showBackground = true, widthDp = 320, heightDp = 320)
 @Composable
-fun OnboardingPreview(): Unit {
-    JetPackComposeTheme {
-        MyApp()
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
+private fun Greetings(
+    names: List<String> = listOf(
+        "위고 요리스", "맷 도허티", "레길론", "로메로", "호이비에르", "산체스", "손흥민",
+        "윙크스", "케인"
+    ), modifier: Modifier = Modifier
+) {
 
 
-    /*remember를 사용하여 변경 가능한 상태를 기억
-    * MutableState는 어떤 값을 보유하고 그 값이 변경될 때마다 UI 업데이트(리컴포지션)하는 인터페이스*/
-    val expanded = remember { mutableStateOf(false) }
-
-    /*항목 펼치기*/
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
-
-    /*Surface는 앱에 넣고자 하는 공통 기능을 처리*/
-    androidx.compose.material.Surface(
-        color = MaterialTheme.colors.primary,
-        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
-
+    // LazyColumn과 LazyRow는 뷰의 RecyclerView와 동일일
+    // LazyColumn은 items 요소 제공
+    LazyColumn(
+        modifier
+            .padding(vertical = 4.dp)
     ) {
-
-        /*Create Row*/
-        Row(modifier = Modifier.padding(24.dp)) {
-            /*Create Column*/
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(bottom = extraPadding) // 밑에 항목 펼치기
-            ) {
-                Text(text = "Hello,")
-                Text(text = name)
-
-            }
-            /*Create OutlinedButton
-            * 상태 변경 및 상태 변경사항에 반응*/
-            OutlinedButton(onClick = { expanded.value = !expanded.value }
-            ) {
-                Text(if (expanded.value) "Show less" else "Show more")
-            }
-
+        items(items = names) { name ->
+            Greeting(name = name)
         }
     }
 }
 
+
+@Composable
+private fun Greeting(name: String, modifier: Modifier = Modifier) {
+
+
+    /*remember를 사용하여 변경 가능한 상태를 기억
+    * MutableState는 어떤 값을 보유하고 그 값이 변경될 때마다 UI 업데이트(리컴포지션)하는 인터페이스*/
+    var expanded by remember { mutableStateOf(false) }
+
+    /*항목 펼치기*/
+    val extraPadding by animateDpAsState(
+        if (expanded) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
+
+    /*Surface는 앱에 넣고자 하는 공통 기능을 처리*/
+    androidx.compose.material.Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp),
+        shape = CircleShape
+    ) {
+
+        /*Create Row*/
+        Row(
+            modifier
+                .padding(24.dp)
+        ) {
+            /*Create Column*/
+            Column(
+                modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding.coerceAtLeast(0.dp)) // 밑에 항목 펼치기
+            ) {
+                Text(text = "Tottenham Hotspur")
+                Text(
+                    text = name,
+                    style = MaterialTheme.typography.h4.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                )
+                if (expanded) {
+                    Text(
+                        text = ("EPL, " + "Tottenham.").repeat(4),
+                    )
+                }
+
+            }
+            IconButton(onClick = { expanded = !expanded }) {
+                Icon(
+                    imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                    contentDescription = if (expanded) {
+                        stringResource(R.string.show_less)
+                    } else {
+                        stringResource(R.string.show_more)
+                    }
+                )
+            }
+        }
+    }
+}
+
+
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     JetPackComposeTheme {
-        MyApp()
+        Greetings()
+    }
+}
+
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview(): Unit {
+    JetPackComposeTheme {
+        OnboardingScreen(onContinueClicked = {})
     }
 }
--- app/src/main/java/com/cookandroid/jetpackcompose/ui/theme/Theme.kt
+++ app/src/main/java/com/cookandroid/jetpackcompose/ui/theme/Theme.kt
@@ -5,17 +5,23 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    surface = Navy,
+    onSurface = Navy,
+    primary = Chartreuse,
+    onPrimary = Navy
+
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+
+    surface = Navy,
+    onSurface = Color.Black,
+    primary = Color.White,
+    onPrimary = Blue
+
 
     /* Other default colors to override
     background = Color.White,
